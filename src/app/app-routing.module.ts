0070db7746cf2434b7f7fba635f214eb0ae30307
deji adesoga <done2stay@gmail.com>
2020-04-23 01:12:48 +0100

    Started Working On Page Design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NoteListComponent } from './pages/note-list/note-list.component';
+import { LayoutComponent } from './structure/layout/layout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LayoutComponent, children : [
+    {path: '', component: NoteListComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoteListComponent } from './pages/note-list/note-list.component';
+import { LayoutComponent } from './structure/layout/layout.component';
+import { NoteCardComponent } from './structure/note-card/note-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteListComponent,
+    LayoutComponent,
+    NoteCardComponent
   ],
   imports: [
     BrowserModule,
